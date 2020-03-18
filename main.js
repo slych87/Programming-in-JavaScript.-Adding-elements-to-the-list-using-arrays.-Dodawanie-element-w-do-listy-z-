65b01eb13b8561d0ed63551edb3b7134a6aa21f9
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-18 10:33:51 +0100

    end

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
-
 const toDoList = [];
 
-const form = document.querySelector('form');
+
+const from = document.querySelector('form');
 const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
 const listItems = document.getElementsByClassName('task');
 const input = document.querySelector('input');
 
 const removeTask = (e) => {
- e.target.parentNode.remove();
- const index = e.target.parentNode.dataset.key;
- toDoList.splice(index, 1)
- console.log(toDoList);
- taskNumber.textContent = listItems.length;
- renderList();
+    e.target.parentNode.remove();
+    const index = e.target.parentNode.dataset.key;
+    toDoList.splice(index, 1)
+    console.log(toDoList);
+    taskNumber.textContent = listItems.length;
+
+    renderList();
 }
 
 
 const addTask = (e) => {
- e.preventDefault()
- const titleTask = input.value;
- if (titleTask === "") return;
- const task = document.createElement('li');
- task.className = 'task';
- task.innerHTML = titleTask + "<button>Usuń</button>";
- toDoList.push(task)
- renderList()
+    e.preventDefault();
+    const titleTask = input.value;
+    if (titleTask === "") return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + "<button>Usuń</button>";
 
- ul.appendChild(task);
- input.value = "";
- // const liItems = document.querySelectorAll('li.task').length;
- taskNumber.textContent = listItems.length;
- task.querySelector('button').addEventListener('click', removeTask);
+    toDoList.push(task);
+    renderList();
+
+    ul.appendChild(task);
+    input.value = "";
+
+    // const liItems = document.querySelectorAll('li.task').length;
+    taskNumber.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask);
 
 }
 
 const renderList = () => {
- ul.textContent = "";
- toDoList.forEach((toDoElement, key) => {
-  toDoElement.dataset.key = key;
-  ul.appendChild(toDoElement);
- })
+    ul.textContent = "";
+    toDoList.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement)
+    })
 }
-form.addEventListener('submit', addTask)
+
+from.addEventListener('submit', addTask);
